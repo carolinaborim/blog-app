403e3e0b132e717ee98111a11c0d993da5e8eabb
Carolina Borim <carolina.borim@gmail.com>
2017-02-28 11:12:06 -0500

    Each post in the list is a link for the post itself

--- src/components/postsIndex.js
+++ src/components/postsIndex.js
@@ -13,27 +13,29 @@ class PostsIndex extends Component{
     return this.props.posts.map((post) => {
       return(
         <li className="list-group-item" key={post.id}>
+          <Link to={"posts/" + post.id}>
           <span className="pull-xs-right">{post.categories}</span>
           <strong>{post.title}</strong>
-        </li>
-      );
-    });
-  }
+        </Link>
+      </li>
+    );
+  });
+}
 
-  render(){
-    return (
-      <div>
-        <div className="text-xs-right">
-          <Link to="/posts/new" className="btn btn-primary">
-            Add a Post
-          </Link>
-        </div>
-        <h3>Posts</h3>
-        <ul className="list-group">
-          {this.renderPosts()}
-        </ul>
-      </div>);
-  }
+render(){
+  return (
+    <div>
+      <div className="text-xs-right">
+        <Link to="/posts/new" className="btn btn-primary">
+        Add a Post
+      </Link>
+    </div>
+    <h3>Posts</h3>
+    <ul className="list-group">
+      {this.renderPosts()}
+    </ul>
+  </div>);
+}
 }
 
 function masStateToProps(state){
