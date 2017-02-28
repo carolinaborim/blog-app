d1122f82874713d1d3e7ad6e84f683233769a3a3
Carolina Borim <carolina.borim@gmail.com>
2017-02-28 10:32:48 -0500

    Uses context from react-router to navigates to the index everytime a new blog post is sucessfully created.

--- src/components/newPosts.js
+++ src/components/newPosts.js
@@ -1,14 +1,27 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {reduxForm} from 'redux-form';
 import {createPost} from '../actions/index';
 import {Link} from 'react-router'
 
 class NewPosts extends Component{
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props){ //this props in here is not this.props - is the props from the form (title, content...)
+      this.props.createPost(props)
+      .then(() => {
+        //blog post has been created, navigate the user to the index
+        // '/' is the new path to navigate to
+        this.context.router.push('/');
+      });
+  }
+
   render(){
     const {fields: {title, categories, content }, handleSubmit} = this.props;
 
     return(
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
 
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : '' }`}>
