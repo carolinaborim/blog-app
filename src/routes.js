46106472f4aaeaf0d1f2fc96afb7a664bcc3dbee
Carolina Borim <carolina.borim@gmail.com>
2017-02-27 19:13:36 -0500

    Creates the new ROUTE and connects to the NewPosts component

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/postsIndex';
+import NewPosts from './components/newPosts';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
+    <Route path="posts/new" component={NewPosts} />
   </Route>
 );
